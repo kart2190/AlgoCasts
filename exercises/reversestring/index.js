89c36e844995cf0cc7d6d81d39b46e04c6b0012d
Karthik <karthik@Karthiks-MacBook-Pro-2.local>
2020-05-14 14:34:02 -0400

    String reverse with three methods

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
 
-module.exports = reverse;
+// <<1>> Using helper methods
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// <<2>> Character to character and new for loop
+// function reverse(str) {
+//     let reversed = '';
+//     for (let charr of str) {
+//         reversed = charr +reversed
+//     }
+//     return reversed
+// }
+
+
+// <<3>> Using reduce method with es6
+// function reverse(str) {
+//  return str.split('').reduce((rev, char) => {
+//      return char + rev;
+//  }, '');
+// }
+
+
+
+module.exports = reverse; 
